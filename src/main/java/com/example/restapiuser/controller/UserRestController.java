eee055c7401d89d88529088475dbc34518895e84
HeoTaehoon7 <greenclass.hoon@gmail.com>
2026-06-29 15:48:44 +0900

    RestApi 완료

--- src/main/java/com/example/restapiuser/controller/UserRestController.java
+++ src/main/java/com/example/restapiuser/controller/UserRestController.java
@@ -3,6 +3,7 @@ package com.example.restapiuser.controller;
 import com.example.restapiuser.dto.DeleteResponse;
 import com.example.restapiuser.dto.UserCreateRequest;
 import com.example.restapiuser.dto.UserResponse;
+import com.example.restapiuser.dto.UserUpdateRequest;
 import com.example.restapiuser.entity.UserEntity;
 import com.example.restapiuser.service.UserService;
 import jakarta.validation.Valid;
@@ -59,6 +60,17 @@ public class UserRestController {
         userService.deleteUser( userid );
         return new DeleteResponse(userid, true);
     }
+
+    // 회원수정  UPDATE
+    // PATCH   PATCH http://localhost:8080/api/users/test01
+    @PatchMapping("/{userid}")
+    public UserResponse update(
+            @PathVariable("userid")  String userid,
+            @Valid @RequestBody UserUpdateRequest request
+    ) {
+        return  userService.updateUser( userid, request );
+    }
+
 
 
 }
